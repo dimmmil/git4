b86316e231772d548b1313b084922326185bc9e1
dimmmil <ivanadimitrova27@gmail.com>
2022-06-21 12:44:36 +0200

    reverting

--- src/git4/ler4.java
+++ src/git4/ler4.java
@@ -6,5 +6,6 @@ public class ler4 {
         System.out.println("automation test ");
         System.out.println("new change");
         System.out.println("amending");
+        System.out.println("reverting");
     }
 }
