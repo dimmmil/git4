5ae9ed3bf1c4a875921d9e25f3bb55085b981aa8
dimmmil <ivanadimitrova27@gmail.com>
2022-06-21 12:17:14 +0200

    amending

--- src/git4/ler4.java
+++ src/git4/ler4.java
@@ -4,5 +4,7 @@ public class ler4 {
     public static void main(String[] args) {
 
         System.out.println("automation test ");
+        System.out.println("new change");
+        System.out.println("amending");
     }
 }
